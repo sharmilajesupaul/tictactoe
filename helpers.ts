8dd9c4bd2e846455a017e0238237983b15070294
Sharmila Jesupaul <tosharmila@gmail.com>
2024-08-03 00:02:09 -0700

    helper discriptions

--- helpers.ts
+++ helpers.ts
@@ -3,18 +3,19 @@ import chalk from 'chalk';
 import Player from './Player.ts';
 import { select } from '@inquirer/prompts';
 
-// add a small delay to make the game more enjoyable
+/** A spinner with a small delay */
 export async function smallDelaySpinner(spinnerText = '', delay = 300) {
   const spinner = ora(spinnerText).start();
   await new Promise((resolve) => setTimeout(resolve, delay));
   spinner.stop();
 }
 
-// a loud log function that highlights the text
+/** A loud log function that underlines and bolds text */
 export function loudLog(str: string) {
   console.log(chalk.underline.bold(` ${str} `));
 }
 
+/** Prompt the user if they want to play again */
 export async function playAgainPrompt() {
   return select({
     message: "Do you want to play again?",
@@ -25,6 +26,7 @@ export async function playAgainPrompt() {
   });
 }
 
+/** Print the current score of both players */
 export function printScore(player1: Player, player2: Player) {
   let winningPlayer: string = player1.getScore() > player2.getScore() ? player1.getName() : player2.getName();
   if (player1.getScore() === player2.getScore()) {
